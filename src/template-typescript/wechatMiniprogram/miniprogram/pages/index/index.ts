af1d3abbfaec3167f7b2df962705f441b93bbcad
Fred <zhangyucheng1207@gmail.com>
2021-06-22 19:57:03 +0800

    wechatMiniprogram: 验证scss功能

--- src/template-typescript/wechatMiniprogram/miniprogram/pages/index/index.ts
+++ src/template-typescript/wechatMiniprogram/miniprogram/pages/index/index.ts
@@ -1,6 +1,6 @@
 // index.ts
 // 获取应用实例
-import { toggleLanguage } from '@utils/i18n'
+import { message, toggleLanguage } from '@utils/i18n'
 const app = getApp<IAppOption>()
 
 Page({
@@ -30,7 +30,7 @@ Page({
     getUserProfile() {
         // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
         wx.getUserProfile({
-            desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+            desc: message('displayUserInfo'), // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
             success: (res) => {
                 console.log(res)
                 this.setData({
