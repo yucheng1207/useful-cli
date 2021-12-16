357599245cae7e71d97e5374704df95904f479fb
Fred <zhangyucheng1207@gmail.com>
2021-12-16 11:35:38 +0800

    react-app: use web-vitals

--- src/template-typescript/react-app/src/index.tsx
+++ src/template-typescript/react-app/src/index.tsx
@@ -1,7 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { getLCP, getFID, getCLS } from 'web-vitals';
 import Home from './pages/Home/index';
 import 'src/styles/app.scss';
+
+getCLS(console.log);
+getFID(console.log);
+getLCP(console.log);
 
 const render = () => {
 	ReactDOM.render(
